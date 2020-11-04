57f5e49702362772623da9c25f5f2714f27a2818
anthuepereira <anthue.pereira@hbsis.com.br>
2020-11-04 19:16:34 -0300

    Correction

--- register.domain/Interfaces/ICustomerRepository.cs
+++ register.domain/Interfaces/ICustomerRepository.cs
@@ -1,15 +1,6 @@
 ﻿using register.domain.Entities;
-using System;
-using System.Collections.Generic;
 
 namespace register.domain.Interfaces
 {
-    public interface ICustomerRepository : IRepository<Customer>
-    {
-        //void Add(Customer customer);
-        //Customer GetById(Guid id);
-        //IEnumerable<Customer> GetAll();
-        //void Remove(Customer customer);
-        //void Update(Customer customer);
-    }
+    public interface ICustomerRepository : IRepository<Customer> { }
 }
